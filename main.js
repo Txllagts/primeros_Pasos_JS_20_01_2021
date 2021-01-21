b82b429a0e1cdcb745b96961ca8d2f62b52fe116
Jasbeith Andrea Prada Hernández <jasbeith2505@gmail.com>
2021-01-20 20:04:04 -0500

    Tipos de datos

---
+++ main.js
@@ -0,0 +1,29 @@
+// Tipos de datos 
+
+// string cadena
+"Jasbeith Andrea"
+'Prada Hernandez'
+
+// number numero
+
+"1000"
+2.5
+
+// boolen 
+true
+false
+
+//array listas
+
+['daniel', 'karol', 'sarai']
+[1, 2, 3, 4]
+[true, false, true, false]
+
+//object objecto
+
+{
+    nombre: "Jasbeith",
+    apellido: "Prada",
+    edad: 16,
+    estudiante: true 
+}
